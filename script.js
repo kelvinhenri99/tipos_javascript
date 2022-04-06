c9dc192ebbdbff72816b231debfe43a6dd0c7587
kelvinhenri99 <kelvin5henri@gmail.com>
2022-04-06 10:13:53 -0300

    Impementação

---
+++ script.js
@@ -0,0 +1,11 @@
+let idade = 23;
+    console.log(typeof idade);
+
+let salario = 4.523;
+    console.log(typeof salario);
+
+let nome = "Kelvin";
+    console.log(typeof nome);
+
+let estaSol = true; // ou false
+    console.log(typeof estaSol);
